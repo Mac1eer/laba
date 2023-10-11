56958edf3bef42c63e0cdd5f7343cf7b8cb66fea
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2023-10-11 08:44:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+Console.WriteLine("Введите ширину прямоугольника");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длинну прямоугольника");
+double b = Convert.ToDouble(Console.ReadLine());
+double P = 2 * (a + b);
+Console.WriteLine($"Перимертр (P) = {P}");
+double S = a * b;
+Console.WriteLine($"Площадь (S) = {S}");
+double d = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+Console.WriteLine($"Диагональ (d) = {d}");
+
+
+
+
+
+
+
+
+
