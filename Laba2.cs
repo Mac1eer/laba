3a4c6a35d1af9fbdb68fda3be60c552a59a1c67a
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2023-10-16 20:33:18 +0300

    Add files via upload

---
+++ Laba2.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите а, b, с");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+if ((a == c || a == b))
+{
+    Console.WriteLine("Треугольник равнобедренный");
+}
+else
+{
+    Console.WriteLine("Треугольник не равнобедренный");
+}
+
+
