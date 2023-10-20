64240d0286452dc2d82057bead79cffe6ea3d58b
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2023-10-20 22:05:17 +0300

    Add files via upload

---
+++ Homework 20 10 23.cs
@@ -0,0 +1,14 @@
+﻿double S = 0;
+for (double i = 0; i <= 1; i += 0.001)
+{
+    S = S + Math.Cos(Math.Pow(2 * i,2));
+}
+Console.WriteLine(S);
+
+
+double H = 0;
+for (double i = 1; i>=0; i -= 0.001)
+{
+    H += Math.Cos(i);
+}
+Console.WriteLine(H);
---
+++ laba3.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите A , B");
+double A =Convert.ToDouble(Console.ReadLine());
+double B = Convert.ToDouble(Console.ReadLine());
+for (double i = A; i <= B; i++)
+{
+    Console.WriteLine(Math.Pow(i ,3));
+}
+
+
+
+
